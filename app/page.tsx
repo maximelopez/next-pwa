8947c903813d0a0a046f4d69f3dd6d127699eb80
Maxime <maxime.lpz@outlook.fr>
2026-01-19 16:43:05 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const MapLocation = dynamic(() => import("@/components/MapLocation"), {
 export default function Page() {
   return (
     <div className="container" style={{ padding: "2rem" }}>
-      <h1>Bienvenue sur mon application Next.js</h1>
+      <h1>Bienvenue sur mon application Next.js 2</h1>
 
       <BatteryStatus />
 
